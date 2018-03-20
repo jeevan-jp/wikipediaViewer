e717f0c640a019ce63b9e349fb421adf8c05179c
jeevan-jp <jiwanprakash65@gmail.com>
2018-03-20 22:20:59 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$('#keyword').focus();
+
+	function searchOnWiki(){
+
+		var key = $("#keyword").val();
+		var api = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + key + "&format=json&callback=?";
+
+		$.ajax({
+			type: "GET",
+			url: api,
+			async: false,
+			dataType: "json",
+			success: function(data){
+				console.log(data);
+				$('#dataList ul').html('');
+				for(var i=0; i<data[1].length; i++)
+				{
+					$("#dataList ul").append("<a href='"+ data[3][i] +"' target='_blank'><li><div class='decoration'><h5>"+ data[1][i] + "</h5><span>" + data[2][i] + "</span></div></li></a>");
+				}
+				$('#search1').hide();
+			},
+			error: function(errorMessage){
+				alert("error!");
+			}
+		})
+	}
+
+	$('#basic-addon1').click(function(){
+		searchOnWiki();
+	});
+
+	$('#search1').click(function(){
+		searchOnWiki();
+	});
+
+	$('#middle').submit(function(e){
+		e.preventDefault();
+		searchOnWiki();
+	});
+
+})
+
